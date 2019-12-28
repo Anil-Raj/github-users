785d2b2f972dd13a11b9e1d9c9a44142af557a55
Anil G <anilraj659@gmail.com>
2019-12-28 14:02:19 +0530

    Add files via upload

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { UserService } from "../service/user.service";
 import { Router, ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-user-detail",
@@ -8,17 +9,14 @@ import { Router, ActivatedRoute } from "@angular/router";
   styleUrls: ["./user-detail.component.css"]
 })
 export class UserDetailComponent implements OnInit {
-  user: Object;
-  repos$: any;
+  user$: Observable<any>;
+  repos$: Observable<any>;
   constructor(private userService: UserService, public route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.userService
-      .fetchUser(this.route.snapshot.paramMap.get("id"))
-      .subscribe(user => {
-        console.log(user);
-        return (this.user = user);
-      });
+    this.user$ = this.userService.fetchUser(
+      this.route.snapshot.paramMap.get("id")
+    );
     this.repos$ = this.userService.fetchRepos(
       this.route.snapshot.paramMap.get("id")
     );
